2ce1ad34798e0a1ea645e14ca2726d8258a0e3b1
skidaru <tgavrilof@gmail.com>
2025-02-03 20:11:13 +0300

    main

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+
 import Phaser from 'phaser'
 import { scenes } from './scenes'
 import './style.css'
--- src/scenes/preload.ts
+++ src/scenes/preload.ts
@@ -1,3 +1,19 @@
 export class preload extends Phaser.Scene{
+    constructor() {
+        super('preload');
+    }
 
+
+    preload(){
+        // @ts-ignore
+        window.bridge.initialize()
+    .then(() => {
+        console.log("NIGGERS");
+        
+    })
+    .catch((error: string) => {
+        console.log(error);
+        
+    })
+    }
 }
